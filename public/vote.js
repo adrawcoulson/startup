00528915821a03eb852f9ae6f6fa8a540fa6581e
Audra Coulson <audrac13@byu.edu>
2023-11-11 21:08:16 -0700

    make a third party call in vote.js "displayRandomDevice"

--- public/vote.js
+++ public/vote.js
@@ -1,3 +1,14 @@
-let parsedUser = JSON.parse(localStorage.getItem("user"));
-parsedUser.name;
-document.getElementById("deviceInfo").innerHTML = parsedUser.name;
+// let parsedUser = JSON.parse(localStorage.getItem("user"));
+//parsedUser.name;
+//document.getElementById("deviceInfo").innerHTML = parsedUser.name;
+  
+  function displayRandomDevice() {
+    fetch('https://random-word-api.herokuapp.com/word')
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.getElementById("deviceInfo");
+        containerEl.textContent = data[0];
+      });
+  }
+  
+  displayRandomDevice();
